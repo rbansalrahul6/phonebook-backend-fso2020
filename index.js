6cbdf096418cc9ecaf2977ed5bd28e90feb49433
bansal-rahul <rahul@proptiger.com>
2020-07-29 12:31:55 +0530

    exercise 3.2

--- index.js
+++ index.js
@@ -33,6 +33,13 @@ let persons = [
       res.json(persons)
   })
 
+  app.get('/info', (req, res) => {
+      res.send(`
+      <p>Phonebook has info for ${persons.length} people</p>
+      <p>${Date()}</p>
+      `)
+  })
+
   const PORT = 3001
   app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`)
