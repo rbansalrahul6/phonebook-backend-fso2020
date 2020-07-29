cb288eb022e980d37dd27a4812ef51f2fc2d9694
bansal-rahul <rahul@proptiger.com>
2020-07-29 12:36:30 +0530

    exercise 3.3

--- index.js
+++ index.js
@@ -40,6 +40,16 @@ let persons = [
       `)
   })
 
+  app.get('/api/persons/:id', (req, res) => {
+      const id = Number(req.params.id)
+      const person = persons.find(p => p.id === id)
+      if (person) {
+          res.json(person)
+      } else {
+          res.status(404).end()
+      }
+  })
+
   const PORT = 3001
   app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`)
